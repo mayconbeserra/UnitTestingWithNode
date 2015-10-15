aa86a94c74ab5cc00822a68c6aed7c645dcc7fae
Maycon Beserra <mayconbeserra@hotmail.com>
2015-10-14 21:32:20 -0300

    Adding more unit tests using Sinon and spies

--- test/sinonTests.js
+++ test/sinonTests.js
@@ -8,12 +8,23 @@ chai.should();
 
 describe('sinon tests', function() {
   var student;
+  var schedule;
 
   beforeEach(function() {
     student = {
       dropClass: function(classId, cb) {
         //do stuff
-        cb();
+        if (!!cb.dropClass) {
+          cb.dropClass();
+        } else {
+          cb();
+        }
+      }
+    };
+
+    schedule = {
+      dropClass: function() {
+        console.log('class dropped');
       }
     };
   });
@@ -25,5 +36,24 @@ describe('sinon tests', function() {
       student.dropClass(1, spy);
       spy.called.should.be.true;
     });
+
+    it('should call the callback and log to the console', function() {
+      function onClassDropped() {
+        console.log('onClassDropped was called');
+      }
+
+      var spy = sinon.spy(onClassDropped);
+
+      student.dropClass(1, spy);
+      spy.called.should.be.true;
+    });
+
+    it('should call the callback even if it is a method of an object', function() {
+      //the code below replaces the schedule.dropClass to be monitored
+      sinon.spy(schedule, 'dropClass');
+      student.dropClass(1, schedule);
+      schedule.dropClass.called.should.be.true;
+    });
   });
+
 });
